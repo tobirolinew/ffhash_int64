12d8a1967df6199a155f0a26fb17b6501d47fc80
Roland Tóbiás <67808693+tobirolinew@users.noreply.github.com>
2025-08-24 14:32:27 -0400

    Update example_multiple_tables.f90
    Tests added for the int64 case.

--- example_multiple_tables.f90
+++ example_multiple_tables.f90
@@ -1,4 +1,5 @@
-module m_hash_a
+module m_hash_a 
+  use iso_fortran_env
   implicit none
 #define FFH_KEY_TYPE integer
 #define FFH_VAL_TYPE character(len=15)
@@ -7,6 +8,7 @@ module m_hash_a
 end module m_hash_a
 
 module m_hash_b
+  use iso_fortran_env
   implicit none
 #define FFH_KEY_TYPE character(len=20)
 #define FFH_KEY_IS_STRING
@@ -14,32 +16,71 @@ module m_hash_b
 #include "ffhash_inc.f90"
 end module m_hash_b
 
+module m_hash_c
+  use iso_fortran_env
+  implicit none
+#define FFH_ENABLE_INT64
+#define FFH_KEY_TYPE integer(int64)
+#define FFH_VAL_TYPE integer(int64)
+#include "ffhash_inc.f90"
+end module m_hash_c
+
+module m_hash_d
+  use iso_fortran_env
+  implicit none
+#define FFH_ENABLE_INT64
+#define FFH_KEY_TYPE integer(int64)
+#define FFH_VAL_TYPE character(len=31)
+#define FFH_VAL_IS_STRING
+#include "ffhash_inc.f90"
+end module m_hash_d
+
 program test
+  use iso_fortran_env, only: int64
   use m_hash_a, ffha_t => ffh_t
   use m_hash_b, ffhb_t => ffh_t
+  use m_hash_c, ffhc_t => ffh_t
+  use m_hash_d, ffhd_t => ffh_t
   implicit none
 
   type(ffha_t) :: ha
   type(ffhb_t) :: hb
+  type(ffhc_t) :: hc
+  type(ffhd_t) :: hd
   integer      :: i
+  integer(int64) :: k64, v64
 
+  ! int -> string
   call ha%ustore_value(1, "hello world")
   print *, ha%fget_value(1)
-  if (ha%fget_value(1) /= "hello world") error stop "FAILED"
+  if (ha%fget_value(1) /= "hello world") error stop "FAILED ha"
 
+  ! string -> int
   call hb%ustore_value("first", 12345)
   print *, hb%fget_value("first")
-  if (hb%fget_value("first") /= 12345) error stop "FAILED"
+  if (hb%fget_value("first") /= 12345) error stop "FAILED hb"
 
-  ! Try to store the same key twice
   call hb%store_key("first", i)
-
-  ! Now an error should be thrown when setting a key twice
   hb%existing_key_is_error = .true.
   call hb%store_key("first", i)
   if (i /= -2) error stop "Expected -2 for duplicate key"
 
+  ! int64 -> int64
+  k64 = 9876543210123456_int64
+  v64 = huge(0_int64) / 2
+  call hc%ustore_value(k64, v64)
+  print *, hc%fget_value(k64)
+  if (hc%fget_value(k64) /= v64) error stop "FAILED hc"
+
+  ! int64 -> string
+  call hd%ustore_value(k64, "64-bit key works")
+  print *, hd%fget_value(k64)
+  if (hd%fget_value(k64) /= "64-bit key works") error stop "FAILED hd"
+
   print *, "PASSED"
+
   call ha%reset()
   call hb%reset()
+  call hc%reset()
+  call hd%reset()
 end program test
